da09fac0fe16308cbd8fc58ff1c21840809283b2
mtianyan <1147727180@qq.com>
2018-01-07 15:30:20 +0800

    >留言板数据库orm映射成表完成。内容截止教程3-3结束。

--- apps/message/models.py
+++ apps/message/models.py
@@ -1,5 +1,36 @@
+# coding: utf-8
 from __future__ import unicode_literals
 
 from django.db import models
 
 # Create your models here.
+
+
+# 继承于django.db.models.Model
+class UserMessage(models.Model):
+    object_id = models.CharField(primary_key=True, max_length=50,default="", verbose_name="主键")
+    # 设置最大长度，verbose_name在后台显示字段会用到
+    name = models.CharField(max_length=20,null=True,blank=True,default="", verbose_name=u"用户名")
+    # Django提供内置的邮箱字段会帮忙验证` default_validators = [validators.validate_email]`
+    email = models.EmailField(verbose_name=u"邮箱")
+    address = models.CharField(max_length=100, verbose_name=u"联系地址")
+    message = models.CharField(max_length=500, verbose_name=u"留言信息")
+
+    class Meta:
+        verbose_name = u"用户留言信息"
+
+        # 指明复数信息，否则后台显示"用户留言s"
+        verbose_name_plural = verbose_name
+
+        # 这里我们让它自动生成所以不用指定db-table
+        # db_table = user_message
+
+        # ordering指定默认排序字段,如：
+        # ordering = ['-object_id']
+
+
+
+
+
+
+
--- apps/message/views.py
+++ apps/message/views.py
@@ -1,6 +1,23 @@
+# coding:utf-8
 from django.shortcuts import render
+# import MySQLdb
 
 # Create your views here.
 
+
 def getform(request):
     return render(request, 'message_form.html')
+
+
+# # 使用原生sql获取书的列表
+# def book_list(request):
+#     # 创建到数据库的连接: 指明用户名，数据库，密码
+#     db = MySQLdb.connect(user = 'me', db='mydb', passwd='secret', host='localhost')
+#     # 创建一个游标对象执行器
+#     cursor = db.cursor()
+#     # 书写我们需要的sql语句
+#     cursor.execute('SELECT name FROM books ORDER BY name')
+#     # 对于fetchall()的结果做遍历，将遍历回来的结果当做数组，取第0个值name。
+#     names = [row[0] for row in cursor.fetchall()]
+#     db.close()
+
